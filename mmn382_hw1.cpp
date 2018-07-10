648a47db6552b554c9e65ec739b72defbb60d12e
naowaff97 <40800929+naowaff97@users.noreply.github.com>
2018-07-10 10:12:40 -0400

    Add files via upload

---
+++ mmn382_hw1.cpp
@@ -0,0 +1,51 @@
+#include "std_lib_facilities.h"
+
+
+
+int main(){
+    cout << "double to int" << endl;
+    double a = 35000.9;
+    int b = a;
+    cout << " a = " << a << ", b = " << b;
+    a = b;
+    cout << ", now a = " << a << endl;
+
+    cout << "double to char" << endl;
+    double c = 35000.9;
+    char d = c;
+    cout << " c = " << c << ", d = " << d;
+    c = d;
+    cout << ", now c = " << c << endl;
+
+    cout << "double to bool" << endl;
+    double e = 35000.9;
+    bool f = e;
+    cout << " e = " << e << ", f = " << f;
+    e = f;
+    cout << ", now e = " << e << endl;
+
+    cout << "int to char" << endl;
+    int g = 35000;
+    char h = g;
+    cout << " g = " << g << ", h = " << h;
+    g = h;
+    cout << ", now g = " << g << endl;
+
+    cout << "int to bool" << endl;
+    int i = 35000;
+    bool j = i;
+    cout << " i = " << i << ", j = " << j;
+    i = j;
+    cout << ", now i = " << i << endl;
+
+    cout << "char to bool" << endl;
+    char k = 'A';
+    bool l = k;
+    cout << " k = " << k << ", l = " << l;
+    k = l;
+    cout << ", now k = " << k << endl;
+
+return 0; }
+
+
+
